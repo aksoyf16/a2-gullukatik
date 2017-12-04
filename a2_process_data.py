ce8dfd53990eae589f65874c0aea4507c85c8d02
Güllü Katık <gullu.katik@inomera.com>
2017-12-04 08:47:09 +0300

    data processing

--- a2_process_data.py
+++ a2_process_data.py
@@ -12,6 +12,102 @@ with open("a2_input.csv") as input_file:
     for row in csv.reader(input_file):
         contents = contents + [row]
 
+total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+count_of_megacities = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+count_of_metropolises = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for j in range(1, 11):
+    for i in range(1, 82):
+        total[j - 1] += int(contents[i][j].replace(' ', ''))
+        if int(contents[i][j].replace(' ', '')) >= 10000000:
+            count_of_megacities[j-1] += 1
+        elif int(contents[i][j].replace(' ', '')) >= 1000000:
+            count_of_metropolises[j-1] += 1
+
+print("""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <style>
+      table, th, td {
+        border: 1px solid black;
+      }
+    </style>
+    <title>Output of my lovely algorithm</title>
+  </head>
+  <body>
+    <table class="population">
+      <tr>
+""")
+for j in range(11):
+    print('        <th>', end='')
+    print(contents[0][j], end='')
+    print('        </th>', end='')
+    print()
+print('      </tr>')
+
+for i in range (1, 82):
+    print('      <tr>')
+    print()
+    for j in range (11):
+        print('        <td>', end='')
+        print(contents[i][j], end='')
+        print('        </td>', end='')
+        print()
+    print('      </tr>')
+    print()
+
+
+print('      <tr>')
+print()
+print('        <td>', end='')
+print('total', end='')
+print('        </td>', end='')
+print()
+for j in range(10):
+    print('        <td>', end='')
+    print(total[j], end='')
+    print('        </td>', end='')
+    print()
+print('      </tr>')
+print()
+
+print('      <tr>')
+print()
+print('        <td>', end='')
+print('number of megacities', end='')
+print('        </td>', end='')
+print()
+for j in range(10):
+    print('        <td>', end='')
+    print(count_of_megacities[j], end='')
+    print('        </td>', end='')
+    print()
+print('      </tr>')
+print()
+
+print('      <tr>')
+print()
+print('        <td>', end='')
+print('number of metropolises', end='')
+print('        </td>', end='')
+print()
+for j in range(10):
+    print('        <td>', end='')
+    print(count_of_metropolises[j], end='')
+    print('        </td>', end='')
+    print()
+print('      </tr>')
+print()
+
+print("""
+      </table>
+    </body>
+</html>
+""")
+
+
 #######################################################
 ### Do your data processing below.
 ### The below code gives some examples
@@ -19,11 +115,3 @@ with open("a2_input.csv") as input_file:
 ### Print your results using the print function.
 #######################################################
 
-print("This assignment (assignment 2) hasn't been finished.")
-print("All it can do is print out the contents of a couple of cells of the file a2_input.csv:")
-print("Cell at index 0,0:")
-print(contents[0][0])
-print("Cell at index 0,1:")
-print(contents[0][1])
-print("Cell at index 1,0:")
-print(contents[1][0])
